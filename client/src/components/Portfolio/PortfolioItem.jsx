98853c0f80d0086b10506bb6e714979407fbe668
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-29 08:48:50 -0500

    adjusted portfolio padding

--- client/src/components/Portfolio/PortfolioItem.jsx
+++ client/src/components/Portfolio/PortfolioItem.jsx
@@ -3,11 +3,11 @@ import './PortfolioItem.css';
 
 const PortfolioItem = ({id, image, title, overview, url}) => {
         return (
-            <div className="portcard col-md-5 shadow-lg p-3 mt-3 mb-1 pt-5 rounded">
-                <h3 className="text-white">{title}</h3>
+            <div className="portcard col-md-5 shadow-lg mt-3 mb-1 rounded">
+                <h3 className="text-white pb-3">{title}</h3>
                 <a href={url} target="__blank"><img src={image} alt={url} className="portImg"></img></a>
                 <br />
-                 <h4 className="projecttext text-white mt-3">{overview}</h4>
+                 <h4 className="projecttext text-white">{overview}</h4>
             </div>
         )
     };
