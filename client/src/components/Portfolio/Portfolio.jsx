253ab59614b178d7d5534fef35c03fb6c0c0bad1
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-29 06:24:28 -0500

    added google books to portfolio

--- client/src/components/Portfolio/Portfolio.jsx
+++ client/src/components/Portfolio/Portfolio.jsx
@@ -9,6 +9,7 @@ import portfolioPicSeven from '../images/4.jpg';
 import portfolioPicEight from '../images/3.jpg';
 import portfolioPicNine from '../images/2.jpg';
 import portfolioPicTen from '../images/15.jpg'
+import portfolioPicEleven from '../images/16.jpg'
 import PortfolioItem from './PortfolioItem.jsx';
 import Container from '../Container/Container.jsx';
 import './Portfolio.css';
@@ -17,116 +18,127 @@ import './Portfolio.css';
 // import PortfolioCard from './PortfolioCard.jsx'
 
 const data = [
-{
-    id: 14,
-    title: "PWA Budget Tracker",
-    bootcamp: true,
-    independent: false,
-    favorite: true,
-    image: portfolioPicOne,
-    url: 'https://budget-trackr.herokuapp.com/',
-    overview:
-    "PWA, Mongo, Node, Express"
-},
-{
-    id: 13,
-    title: "Employee Directory",
-    bootcamp: true,
-    independent: false,
-    favorite: true,
-    image: portfolioPicTwo,
-    url: 'https://vast-inlet-61488.herokuapp.com/',
-    overview:
-        "React, React Hooks, axios"
+    {
+        id: 16,
+        title: "Google Books Search & Save",
+        bootcamp: true,
+        independent: true,
+        favorite: true,
+        image: portfolioPicEleven,
+        url: 'https://react-google-books-app.herokuapp.com/',
+        overview:
+        "MERN, backwards compatible, axios, mongoose"
     },
     {
-    id: 11,
-    title: "Workout Tracker",
-    bootcamp: true,
-    independent: false,
-    favorite: true,
-    image: portfolioPicThree,
-    url: 'https://exercise-trackr.herokuapp.com/',
-    overview:
-        "Mongo, Mongoose, Express, Node"
+        id: 14,
+        title: "PWA Budget Tracker",
+        bootcamp: true,
+        independent: false,
+        favorite: true,
+        image: portfolioPicOne,
+        url: 'https://budget-trackr.herokuapp.com/',
+        overview:
+        "PWA, Mongo, Node, Express"
     },
     {
-    id: 7,
-    title: "Rec-reate",
-    bootcamp: true,
-    independent: true,
-    favorite: true,
-    image: portfolioPicFour,
-    url: 'https://rec-reate.herokuapp.com/',
-    overview:
-        "Project Two; MySQL, Sequelize, Handlebars, Express, Node, Passport, Bcrypt"
-    },
-    {
-    id: 10,
-    title: "Weather Dashboard",
-    bootcamp: true,
-    independent: false,
-    favorite: false,
-    image: portfolioPicFive,
-    url: 'https://dopaminedriven.github.io/WeatherDashboard/',
-    overview:
-        "API, AJAX, JavaScript, jQuery"
-    },
-    {
-    id: 8,
-    title: "Burger App",
-    bootcamp: true,
-    independent: false,
-    favorite: false,
-    image: portfolioPicSix,
-    url: 'https://infinite-stream-25057.herokuapp.com/',
-    overview:
-        "MySQL, Custom ORM, Handlebars, Node, Express, jQuery"
-    },
-    {
-    id: 4,
-    title: "Express Note Taker",
-    bootcamp: true,
-    independent: false,
-    favorite: false,
-    image: portfolioPicSeven,
-    url: 'https://express-note-taking-app.herokuapp.com/',
-    overview:
-        "Express, node, local storage, jQuery"
-    },
-    {
-    id: 3,
-    title: "Employee Roster Generator",
-    bootcamp: true,
-    independent: false,
-    favorite: false,
-    image: portfolioPicEight,
-    url: 'https://www.youtube.com/watch?v=1aqZo6awj38',
-    overview:
-        "Node CLI, Jest, MySQL, Inquirer"
-    },
-    {
-    id: 2,
-    title: "GitHub Profile Generator",
-    bootcamp: true,
-    independent: false,
-    favorite: false,
-    image: portfolioPicNine,
-    url: 'https://www.youtube.com/watch?v=T1ln-xt-Dm4',
-    overview:
-        "Node CLI, Axios, Electron"
-    },
-    {
-    id: 15,
-    title: "Resume",
-    bootcamp: false,
-    independent: true,
-    favorite: false,
-    image: portfolioPicTen,
-    url: "https://docs.google.com/document/d/1hnT5DnJnkSmGOvhXNyA2hZdSdY8tODg8qA8xsjPy0vc/edit?usp=sharing",
-    overveiw: "Overview"
-    }
-];
+        id: 13,
+        title: "Employee Directory",
+        bootcamp: true,
+        independent: false,
+        favorite: true,
+        image: portfolioPicTwo,
+        url: 'https://vast-inlet-61488.herokuapp.com/',
+        overview:
+            "React, React Hooks, axios"
+        },
+        {
+        id: 11,
+        title: "Workout Tracker",
+        bootcamp: true,
+        independent: false,
+        favorite: true,
+        image: portfolioPicThree,
+        url: 'https://exercise-trackr.herokuapp.com/',
+        overview:
+            "Mongo, Mongoose, Express, Node"
+        },
+        {
+        id: 7,
+        title: "Rec-reate",
+        bootcamp: true,
+        independent: true,
+        favorite: true,
+        image: portfolioPicFour,
+        url: 'https://rec-reate.herokuapp.com/',
+        overview:
+            "Project Two; MySQL, Sequelize, Handlebars, Express, Node, Passport, Bcrypt"
+        },
+        {
+        id: 10,
+        title: "Weather Dashboard",
+        bootcamp: true,
+        independent: false,
+        favorite: false,
+        image: portfolioPicFive,
+        url: 'https://dopaminedriven.github.io/WeatherDashboard/',
+        overview:
+            "API, AJAX, JavaScript, jQuery"
+        },
+        {
+        id: 8,
+        title: "Burger App",
+        bootcamp: true,
+        independent: false,
+        favorite: false,
+        image: portfolioPicSix,
+        url: 'https://infinite-stream-25057.herokuapp.com/',
+        overview:
+            "MySQL, Custom ORM, Handlebars, Node, Express, jQuery"
+        },
+        {
+        id: 4,
+        title: "Express Note Taker",
+        bootcamp: true,
+        independent: false,
+        favorite: false,
+        image: portfolioPicSeven,
+        url: 'https://express-note-taking-app.herokuapp.com/',
+        overview:
+            "Express, node, local storage, jQuery"
+        },
+        {
+        id: 3,
+        title: "Employee Roster Generator",
+        bootcamp: true,
+        independent: false,
+        favorite: false,
+        image: portfolioPicEight,
+        url: 'https://www.youtube.com/watch?v=1aqZo6awj38',
+        overview:
+            "Node CLI, Jest, MySQL, Inquirer"
+        },
+        {
+        id: 2,
+        title: "GitHub Profile Generator",
+        bootcamp: true,
+        independent: false,
+        favorite: false,
+        image: portfolioPicNine,
+        url: 'https://www.youtube.com/watch?v=T1ln-xt-Dm4',
+        overview:
+            "Node CLI, Axios, Electron"
+        },
+        {
+        id: 15,
+        title: "Resume",
+        bootcamp: false,
+        independent: true,
+        favorite: false,
+        image: portfolioPicTen,
+        url: "https://docs.google.com/document/d/1hnT5DnJnkSmGOvhXNyA2hZdSdY8tODg8qA8xsjPy0vc/edit?usp=sharing",
+        overveiw: "Overview"
+        }
+    ];
 
 const Portfolio = () => {
         return (
